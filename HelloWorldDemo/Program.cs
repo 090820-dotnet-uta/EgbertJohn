d03219e5363ed757e9a202526a4fde599594ff51
jhnegbrt <jhnegbrt@gmail.com>
2020-09-08 18:38:56 -0400

    added middle initial to Name

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello John Egbert!");
+            Console.WriteLine("Hello John W Egbert!");
             System.Console.WriteLine("What is your name?");
             string input = Console.ReadLine();
             System.Console.WriteLine($"Hello {input}");
